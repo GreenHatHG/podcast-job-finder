bcad0412c41d63a50c37b6293837ebb6a2f392ae
GreenHatHG <greenhat2333@gmail.com>
2026-07-06 16:47:25 +0800

    Add local static analysis tooling

---
+++ vulture_whitelist.py
@@ -0,0 +1,10 @@
+"""Whitelist for code usage that Vulture cannot infer statically."""
+
+from extract_xiaoyuzhou_episode import _HTMLTextExtractor
+
+
+VULTURE_WHITELIST_REFERENCES = (
+    _HTMLTextExtractor.handle_data,
+    _HTMLTextExtractor.handle_starttag,
+    _HTMLTextExtractor.handle_endtag,
+)
